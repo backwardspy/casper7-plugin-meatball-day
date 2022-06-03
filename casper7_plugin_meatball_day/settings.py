cafccc636e5936e02fb98e6bb5d8cd1c1dd7ca50
backwardspy <backwardspy@gmail.com>
2022-06-03 09:40:01 +0100

    :card_file_box: fix(db): make db config portable

--- casper7_plugin_meatball_day/settings.py
+++ casper7_plugin_meatball_day/settings.py
@@ -8,7 +8,9 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     """Top level settings."""
 
-    database: Path = Path(platformdirs.user_data_dir(__package__)) / "meatball.db"
+    meatball_database: Path = (
+        Path(platformdirs.user_data_dir(__package__)) / "meatball.db"
+    )
 
 
 settings = Settings()
--- piccolo_conf.py
+++
@@ -1,6 +0,0 @@
-"""Piccolo database engine configuration."""
-from piccolo.engine import SQLiteEngine
-
-from casper7_plugin_meatball_day.settings import settings
-
-DB = SQLiteEngine(str(settings.database))
